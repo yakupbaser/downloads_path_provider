374f89256c30a015824796d32c743b261d486d53
Yakup BAŞER <yakupbaser@hotmail.com.tr>
2021-05-27 11:11:16 +0300

    added null-safety

--- lib/downloads_path_provider.dart
+++ lib/downloads_path_provider.dart
@@ -4,14 +4,10 @@ import 'dart:io';
 import 'package:flutter/services.dart';
 
 class DownloadsPathProvider {
-  static const MethodChannel _channel =
-      const MethodChannel('downloads_path_provider');
+  static const MethodChannel _channel = const MethodChannel('downloads_path_provider');
 
   static Future<Directory> get downloadsDirectory async {
     final String path = await _channel.invokeMethod('getDownloadsDirectory');
-    if (path == null) {
-      return null;
-    }
     return Directory(path);
   }
 }
